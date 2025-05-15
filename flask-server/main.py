22d8caf965014773924a5ae40f64f67b9d9de1a7
arjav1528 <arjav158@gmail.com>
2025-05-15 21:12:26 +0530

    init commit again

---
+++ flask-server/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import os
+import json
+import requests
+
+PORT=os.environ.get('PORT')
+print("PORT:", PORT)
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello, World!"
+
+
+
+if __name__ == '__main__':
+    app.run(port=PORT, debug=True)
